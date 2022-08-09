f52a634ed2c63c5c10643aa145caa8d02e4150c1
Yaroslav Shatkevich <jarek@thestory.pl>
2022-08-09 13:05:00 +0200

    Implement react-ga library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
+
+import ReactGA from 'react-ga';
 import { appWithTranslation } from "next-i18next";
+
+const googleAnalyticsId = process.env.NEXT_PUBLIC_GA_ID;
+
+if (googleAnalyticsId) {
+  ReactGA.initialize(googleAnalyticsId);
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
