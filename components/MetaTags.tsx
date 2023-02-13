8a9d5c4515b08caefe9288653a50102a62c3d651
Yaroslav Shatkevich <jarek@thestory.pl>
2023-02-13 19:03:02 +0100

    Clean up
    * Add robots.txt and favicon.ico
    * Fix types

--- components/MetaTags.tsx
+++ components/MetaTags.tsx
@@ -1,31 +1,27 @@
+import type { OpenGraphMedia } from "next-seo/lib/types";
 import type { ApiSeo } from "../schemas";
 
 import { NextSeoProps, NextSeo } from "next-seo";
 import { useRouter } from "next/router";
-
 import React from "react";
 
 import { mediaUrl } from "../lib/strapi";
-
-interface MetaImage {
-  url: string;
-}
 
 export const MetaTags: React.FC<ApiSeo> = ({ metaImage, ...props }) => {
   const { locale } = useRouter();
 
   const nextSeoProps = {
     openGraph: {
-      images: [] as MetaImage[],
+      images: [] as OpenGraphMedia[],
       locale
     },
     title: props.metaTitle,
     description: props.metaDescription,
     keywords: props.keywords,
     canonical: props.canonicalURL,
-  };
+  } as NextSeoProps;
 
-  if (metaImage) {
+  if (metaImage && nextSeoProps.openGraph) {
     nextSeoProps.openGraph.images = [{ url: mediaUrl(metaImage.data.attributes.url) }];
   }
 
